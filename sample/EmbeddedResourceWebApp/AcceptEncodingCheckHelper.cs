b4b33e150f83d274a129919a978cff548cad1d00
stratos <stratosblue@outlook.com>
2025-05-10 18:23:23 +0800

    chore: add embedded resource web app sample

---
+++ sample/EmbeddedResourceWebApp/AcceptEncodingCheckHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Primitives;
+
+public static partial class AcceptEncodingCheckHelper
+{
+    private static readonly Regex s_gzipAcceptedCheckRegex = GetGZipAcceptedCheckRegex();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsGZipAccepted(string? acceptEncoding) => !string.IsNullOrWhiteSpace(acceptEncoding) && s_gzipAcceptedCheckRegex.IsMatch(acceptEncoding);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsGZipAccepted(in StringValues acceptEncodingValues)
+    {
+        return acceptEncodingValues.Count switch
+        {
+            0 => false,
+            1 => IsGZipAccepted(acceptEncodingValues[0]),
+            _ => SlowCheckIsGZipAccepted(in acceptEncodingValues),
+        };
+    }
+
+    [GeneratedRegex(@"(^|,)\s*gzip\s*(;|,|$)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+    private static partial Regex GetGZipAcceptedCheckRegex();
+
+    private static bool SlowCheckIsGZipAccepted(in StringValues values)
+    {
+        var valuesCount = values.Count;
+        for (var i = 0; i < valuesCount; i++)
+        {
+            if (IsGZipAccepted(values[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ sample/EmbeddedResourceWebApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO.Compression;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+var resourceAssembly = typeof(GZipCompressedEmbeddedFileProvider).Assembly;
+
+//decompress gzip file provider
+app.UseStaticFiles(new StaticFileOptions()
+{
+    RequestPath = "/location1",
+    FileProvider = new GZipCompressedEmbeddedFileProvider(resourceAssembly, "EmbeddedResourceWebApp.Resources.html")
+});
+
+//directly write
+app.MapGet("/location2/rfc7230.htm", async context =>
+{
+    using var contentStream = resourceAssembly.GetManifestResourceStream("EmbeddedResourceWebApp.Resources.html.rfc7230.htm")!;
+
+    if (AcceptEncodingCheckHelper.IsGZipAccepted(context.Request.Headers.AcceptEncoding))
+    {
+        context.Response.Headers.ContentEncoding = "gzip";
+        await contentStream.CopyToAsync(context.Response.Body, context.RequestAborted);
+    }
+    else
+    {
+        using var gzipStream = new GZipStream(contentStream, CompressionMode.Decompress);
+        await gzipStream.CopyToAsync(context.Response.Body, context.RequestAborted);
+    }
+});
+
+app.Run();
